01fd1fa81465ee8c926112ed9fdca79ef0875f75
Alex Clark <aclark@aclark.net>
2019-08-06 17:49:43 -0400

    Update

--- application.py
+++
@@ -1,3 +0,0 @@
-def application(environ, start_fn):
-    start_fn('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1>Welcome to the Forum of ESRD Networks Website</h1><h2>ESRD Network will be right back!</h2>\n"]
